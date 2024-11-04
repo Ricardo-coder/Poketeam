03d34441e94c3c60bec359db6fce1b8eba7d294d
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-04 20:33:51 -0300

    style(logo): logo responsiveness
    add responsiveness styles to logo

--- src/components/SSR/Logo/index.tsx
+++ src/components/SSR/Logo/index.tsx
@@ -4,17 +4,17 @@ import Image from 'next/image'
 export function Logo() {
   return (
     <Section className="flex items-center justify-center">
-      <Spam className="text-5xl font-display -mr-4 z-10 text-yellow text-shadow-lg shadow-blue">
+      <Spam className="text-3xl md:text-5xl font-display -mr-4 z-10 text-yellow text-shadow-lg shadow-blue">
         Poké
       </Spam>
       <Image
-        className="z-0"
+        className="z-0 w-16 md:w-24"
         src="/svg/pokeball.svg"
         alt="Logo"
-        width={80}
-        height={80}
+        width={90}
+        height={90}
       />
-      <Spam className="text-5xl font-display -ml-4 z-10 text-blue text-shadow-lg shadow-yellow">
+      <Spam className="text-3xl md:text-5xl font-display -ml-4 z-10 text-blue text-shadow-lg shadow-yellow">
         Team
       </Spam>
     </Section>
