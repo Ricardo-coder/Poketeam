7e410681d118518fde480c57cb4a6454b98db8bd
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-04 20:18:01 -0300

    feat(logo): logo component
    Add logo component and tailwind text-shadow plugin

--- src/components/SSR/Logo/index.tsx
+++ src/components/SSR/Logo/index.tsx
@@ -1,7 +1,22 @@
+import { Section, Spam } from '@/components/fabrics'
+import Image from 'next/image'
+
 export function Logo() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Logo</h1>
-    </div>
+    <Section className="flex items-center justify-center">
+      <Spam className="text-5xl font-display -mr-4 z-10 text-yellow text-shadow-lg shadow-blue">
+        Poké
+      </Spam>
+      <Image
+        className="z-0"
+        src="/svg/pokeball.svg"
+        alt="Logo"
+        width={80}
+        height={80}
+      />
+      <Spam className="text-5xl font-display -ml-4 z-10 text-blue text-shadow-lg shadow-yellow">
+        Team
+      </Spam>
+    </Section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -20,8 +21,26 @@ const config: Config = {
         yellow: '#FFCC00',
         blue: '#0075BE',
       },
+      textShadow: {
+        sm: '2px 0 var(--tw-shadow-color), -2px 0 var(--tw-shadow-color), 0 2px var(--tw-shadow-color), 0 -2px var(--tw-shadow-color), 1px 1px var(--tw-shadow-color), -1px -1px var(--tw-shadow-color), 1px -1px var(--tw-shadow-color), -1px 1px var(--tw-shadow-color)',
+        DEFAULT:
+          '3px 0 var(--tw-shadow-color), -3px 0 var(--tw-shadow-color), 0 3px var(--tw-shadow-color), 0 -3px var(--tw-shadow-color), 2px 2px var(--tw-shadow-color), -2px -2px var(--tw-shadow-color), 2px -2px var(--tw-shadow-color), -2px 2px var(--tw-shadow-color)',
+        lg: '4px 0 var(--tw-shadow-color), -4px 0 var(--tw-shadow-color), 0 4px var(--tw-shadow-color), 0 -4px var(--tw-shadow-color), 3px 3px var(--tw-shadow-color), -3px -3px var(--tw-shadow-color), 3px -3px var(--tw-shadow-color), -3px 3px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function (api) {
+      const { matchUtilities, theme } = api
+      matchUtilities(
+        {
+          'text-shadow': (value: string) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') },
+      )
+    }),
+  ],
 }
 export default config
