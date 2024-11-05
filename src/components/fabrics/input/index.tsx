46b331a4879db91a612ae06e2e497a5b894573d2
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-04 21:09:24 -0300

    docs(input): add input doc

--- src/components/fabrics/input/index.tsx
+++ src/components/fabrics/input/index.tsx
@@ -1,3 +1,10 @@
+/**
+ * A functional component that renders an input element.
+ *
+ * @param {React.InputHTMLAttributes<HTMLInputElement>} props - The properties passed to the input element.
+ * @param {React.ChangeEventHandler<HTMLInputElement>} props.onChange - The event handler for the input's change event.
+ * @returns {JSX.Element} The rendered input element.
+ */
 import React, { createElement } from 'react'
 
 export function Input({
